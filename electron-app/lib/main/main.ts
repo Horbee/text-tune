6646a917290a4a351e96bc8dddd765af21649529
Horbee <n.horox@gmail.com>
2025-10-16 10:03:17 +0200

    Add single instance lock for windows

--- electron-app/lib/main/main.ts
+++ electron-app/lib/main/main.ts
@@ -8,59 +8,73 @@ import {
   initServices,
   registerAppIPC,
   getConfigService,
+  createOrShowWindow,
 } from './app'
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.horbee.text-tune')
 
-  // Init services
-  initServices()
+// Single instance lock - prevent multiple instances
+const gotTheLock = app.requestSingleInstanceLock()
 
-  registerAppIPC()
-  // Create app window
-  createAppWindow()
-  createTray()
-  globalShortcut.register('F9', fixCurrentLine)
-  globalShortcut.register('F10', fixSelection)
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', () => {
+    // Someone tried to run a second instance, we should focus our window instead.
+    createOrShowWindow()
   })
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createAppWindow()
-    }
-  })
-})
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(() => {
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.horbee.text-tune')
 
-// Since we want to minimize the app to the tray, we don't quit the app when all windows are closed.
-app.on('window-all-closed', () => {
-  const configService = getConfigService()
-  if (!configService.isBackgroundNotificationShown()) {
-    // TODO: refactor notification to use NotificationService
-    new Notification({
-      title: 'Text Tune',
-      body: 'Text Tune is running in the background.',
+    // Init services
+    initServices()
+
+    registerAppIPC()
+    // Create app window
+    createAppWindow()
+    createTray()
+    globalShortcut.register('F9', fixCurrentLine)
+    globalShortcut.register('F10', fixSelection)
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window)
     })
-      .on('click', () => {
-        createAppWindow()
-      })
-      .show()
 
-    configService.setBackgroundNotificationShown(true)
-  }
-  // if (process.platform !== 'darwin') {
-  //   app.quit()
-  // }
-})
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createAppWindow()
+      }
+    })
+  })
+
+  // Since we want to minimize the app to the tray, we don't quit the app when all windows are closed.
+  app.on('window-all-closed', () => {
+    const configService = getConfigService()
+    if (!configService.isBackgroundNotificationShown()) {
+      // TODO: refactor notification to use NotificationService
+      new Notification({
+        title: 'Text Tune',
+        body: 'Text Tune is running in the background.',
+      })
+        .on('click', () => {
+          createAppWindow()
+        })
+        .show()
+
+      configService.setBackgroundNotificationShown(true)
+    }
+    // if (process.platform !== 'darwin') {
+    //   app.quit()
+    // }
+  })
+}
 
 // In this file, you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
